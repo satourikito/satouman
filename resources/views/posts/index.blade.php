6c87e79714164ea046be75e34c4e0d146cf91c8d
satourikito <rikito1130@icloud.com>
2023-08-04 21:40:19 +0900

    ８−３

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,24 +7,26 @@
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-       
 
-        
+
+
     </head>
     <body class="antialiased">
         <h1>Blog Name</h1>
         <div class='posts'>
             @foreach($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                   
+                    <h2 class='title'><a href="/posts/{{ $post->id}}">{{ $post->title }}</a></h2>
+                   
                     <p class='body'>{{ $post->body }}</p>
                 </div>
-            
+
             @endforeach
         </div>
         <div class='Paginate'>
             {{$posts->links()}}
         </div>
-        
+
     </body>
 </html>
